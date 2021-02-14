5e9fd93dc276394624b7301139c8242b04dc0264
astra-bigdata <52942660+astra-bigdata@users.noreply.github.com>
2021-02-14 23:39:31 +0700

    first init

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Optional
+import pandas as pd
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/data")
+def read_root():
+    data = pd.read_csv('addresses.csv')
+    data = data.to_list()
+    return {"datas": data}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
